772d0fe7c0611628edeb6f902ab6864aa682b1f9
Ryan Kelley <kelleyr@duvalschools.org>
2021-10-29 13:03:26 -0400

    Version 0.1
    Added float, string, and int variables.

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,1 +1,11 @@
-# Variables Pratice, Ryan Kelley, 10/29/21 12:48pm, version 0.0
+# Variables Pratice, Ryan Kelley, 10/29/21 1:01pm, version 0.1
+
+
+# Basic Variables 
+
+my_int = -15 # INTEGERS, any +/- whole number and 0. 
+my_float = 1.234567 # FLOATING POINT, any +/- number with a decimal. 
+my_string = "How much wood could a wood chuck chuck if a wood chuck could chuck wood?"
+
+
+
