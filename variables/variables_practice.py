4e8a5c1ac3796d9c98985f33f4ddac20908e1d81
Ryan Kelley <kelleyr@duvalschools.org>
2021-10-29 13:08:21 -0400

    Update variables_practice.py

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -7,5 +7,7 @@ my_int = -15 # INTEGERS, any +/- whole number and 0.
 my_float = 1.234567 # FLOATING POINT, any +/- number with a decimal. 
 my_string = "How much wood could a wood chuck chuck if a wood chuck could chuck wood?"
 
-
+print(my_int)
+print(my_float)
+print(my_string) 
 
